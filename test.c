539de5034df9a808f6479782c81511d86e186c1f
Nazo69 <Nazaretsanosyan@gmail.com>
2025-02-05 14:48:36 +0100

    ajout des fonctions

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu ;
+
+int valeur ;
+
+int main () 
+{
+
+
+
+return 0;
+}
+
+
+void menu
+{
+	printf("Pour afficher l'addresse IP, veuillez tapez 1.\n");
+	printf("Pour afficher la table de routage, veuillez tapez 2.\n");
+	printf("Pour afficher la connectivité avec Google, veuillez tapez 3.\n");
+	printf("Pour quitter le menu, veuillez tapez 0.\n");
